e0da019507e127419a8c337b1b0f1ace42367486
Yuri Canuto <yuriasc@gmail.com>
2018-11-18 02:37:18 -0300

    Identando Código

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cliente;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,55 +7,51 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-/**
- *
- * @author yuria
- */
 public class Cliente {
 
-	public static void main(String[] args) {
-		try {
-			final Socket cliente = new Socket("127.0.0.1", 9090);
-			// lendo mensagem do servidor
-			new Thread() {
-				public void run() {
-					try {
-						BufferedReader leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+    public static void main(String[] args) {
+        try {
+            final Socket cliente = new Socket("127.0.0.1", 9090);
+            // lendo mensagem do servidor
+            new Thread() {
+                public void run() {
+                    try {
+                        BufferedReader leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
 
-						while (true) {
-							String mensagem = leitor.readLine();
-							System.out.println(mensagem);
-						}
+                        while (true) {
+                            String mensagem = leitor.readLine();
+                            System.out.println(mensagem);
+                        }
 
-					} catch (IOException e) {
-						System.out.println("Impossivel ler a mensagem do servidor");
-						e.printStackTrace();
-					}
+                    } catch (IOException e) {
+                        System.out.println("Impossivel ler a mensagem do servidor");
+                        e.printStackTrace();
+                    }
 
-				}
-			}.start();
-			// escrevendo para o servidor
-			PrintWriter escritor = new PrintWriter(cliente.getOutputStream(), true);
-			BufferedReader leitorTerminal = new BufferedReader(new InputStreamReader(System.in));
-			String mensagemTerminal = "";
-			while (true) {
-				mensagemTerminal = leitorTerminal.readLine();
-				if (mensagemTerminal == null || mensagemTerminal.length() == 0) {
-					System.exit(0);
-				}
-				escritor.println(mensagemTerminal);
-				if (mensagemTerminal.equalsIgnoreCase("bye")) {
-					System.exit(0);
-				}
-			}
+                }
+            }.start();
+            // escrevendo para o servidor
+            PrintWriter escritor = new PrintWriter(cliente.getOutputStream(), true);
+            BufferedReader leitorTerminal = new BufferedReader(new InputStreamReader(System.in));
+            String mensagemTerminal = "";
+            while (true) {
+                mensagemTerminal = leitorTerminal.readLine();
+                if (mensagemTerminal == null || mensagemTerminal.length() == 0) {
+                    System.exit(0);
+                }
+                escritor.println(mensagemTerminal);
+                if (mensagemTerminal.equalsIgnoreCase("bye")) {
+                    System.exit(0);
+                }
+            }
 
-		} catch (UnknownHostException e) {
-			System.err.println("O cliente fechou a conexão");
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        } catch (UnknownHostException e) {
+            System.err.println("O cliente fechou a conexão");
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
